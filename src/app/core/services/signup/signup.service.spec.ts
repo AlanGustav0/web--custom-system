9c998d2f1b3ed8ac8dc510b26dc5cf97882432a5
AlanGustav0 <alagustavo946@gmail.com>
2023-04-24 20:40:53 -0300

    test: fix spec file on menu and signup component

--- src/app/core/services/signup/signup.service.spec.ts
+++ src/app/core/services/signup/signup.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SignupService } from './sigunp.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SignupService', () => {
   let service: SignupService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(SignupService);
   });
 
