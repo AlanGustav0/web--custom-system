6e7c935f83c81b5dbb41570445e28a2c9177540b
AlanGustav0 <alagustavo946@gmail.com>
2023-04-25 08:46:17 -0300

    test: coverage unit tests menu.component

--- src/app/core/menu/menu.component.spec.ts
+++ src/app/core/menu/menu.component.spec.ts
@@ -3,10 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuComponent } from './menu.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { OverlayPanelModule } from 'src/app/shared/components/overlay/overlay-panel.module';
+import { Router } from '@angular/router';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,6 +17,7 @@ describe('MenuComponent', () => {
     })
     .compileComponents();
 
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,5 +25,29 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`#${MenuComponent.prototype.activeMenu.name} should be set style (active) in overlayMenu when it has been called)`,() => {
+    const icon = fixture.nativeElement.querySelector('.nav__container__menu-mobile__icon');
+    const overlayMenu = fixture.nativeElement.querySelector('#overlay-menu');
+    icon.click();
+    fixture.detectChanges();
+    expect(overlayMenu.classList.contains('active')).toBe(true);
+  });
+
+  it(`#${MenuComponent.prototype.activeMenu.name} should be set faBars in iconBars when it has been called`,() => {
+    const icon = fixture.nativeElement.querySelector('.nav__container__menu-mobile__icon');
+    icon.click();
+    fixture.detectChanges();
+    icon.click();
+    expect(component.iconBars.iconName).toEqual('bars');
+  });
+
+  it(`#${MenuComponent.prototype.logout.name} should be navigate to (/boas-vindas) when it has been called`,() => {
+    const spyRouter = spyOn(router,'navigate');
+    const logoutButton = fixture.nativeElement.querySelector('#logout');
+    logoutButton.click();
+    fixture.detectChanges();
+    expect(spyRouter).toHaveBeenCalledWith(['/boas-vindas']);
   });
 });
