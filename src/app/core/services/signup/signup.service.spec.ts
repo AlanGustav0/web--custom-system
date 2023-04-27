26d65086a3cf00f6a0d01d477d9d1561f08d40f2
AlanGustav0 <alagustavo946@gmail.com>
2023-04-27 11:52:47 -0300

    test: Coverage unit tests signup.service.spec.ts

--- src/app/core/services/signup/signup.service.spec.ts
+++ src/app/core/services/signup/signup.service.spec.ts
@@ -1,19 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SignupService } from './sigunp.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { SignupRequest } from '../interfaces/request/signup-request.interface';
 
-describe('SignupService', () => {
+describe(SignupService.name, () => {
   let service: SignupService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[HttpClientTestingModule]
     });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(SignupService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it(`#${SignupService.prototype.signup.name} should be call (POST) method when it has been called`, () => {
+    const request: SignupRequest = {
+      userName: 'user',
+      password: 'password',
+    };
+    service
+      .signup(request)
+      .subscribe((data) => expect(data).not.toBeNull);
+
+    const req = httpTestingController.expectOne('http://localhost:5038/usuario/cadastrar');
+    expect(req.request.method).toEqual('POST');
+    req.flush(request);
+    httpTestingController.verify();
   });
 });
