45e59f2abef9c35326238530dfc55547f6358dab
AlanGustav0 <alagustavo946@gmail.com>
2023-05-04 07:45:45 -0300

    feature: Implements canLoad guard on login

--- src/app/core/services/auth/auth.guard.ts
+++ src/app/core/services/auth/auth.guard.ts
@@ -1,36 +1,30 @@
 import { Injectable } from '@angular/core';
 import {
-  ActivatedRouteSnapshot,
   CanActivate,
   Router,
-  RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from '../user/user.service';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly _userService: UserService,
     private readonly _router: Router
   ) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
+  canActivate():
     | boolean
     | UrlTree
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree> {
+    if (!this._userService.isLogged()) {
+      this._router.navigate(['']);
+      return false;
+    }
 
-      if(!this._userService.isLogged()){
-        this._router.navigate(['']);
-        return false;
-      }
-
-      return true;
+    return true;
   }
 }
---
+++ src/app/core/services/auth/login.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '../token/token.service';
+@Injectable({ providedIn: 'root' })
+export class LoginGuard implements CanLoad {
+  constructor(
+    private readonly _tokenService: TokenService,
+    private readonly _router: Router
+  ) {}
+
+  canLoad():
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+
+    if (this._tokenService.hasToken()) {
+      this._router.navigate(['/home']);
+      return false;
+    }
+
+    return true;
+  }
+}
