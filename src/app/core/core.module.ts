ff00d39748f1be80fcb98c51b604f2f65d7e5e21
AlanGustav0 <alagustavo946@gmail.com>
2023-06-15 13:50:30 -0300

    refactor: menu.component in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,18 +4,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { OverlayPanelModule } from '../shared/components/overlay/overlay-panel.module';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MenuComponent],
-  exports:[MenuComponent],
+  exports: [MenuComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     FontAwesomeModule,
-    OverlayPanelModule
-
-  ]
+    OverlayPanelModule,
+    RouterModule,
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
