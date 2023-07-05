3855aa2cf6f847f069e71dc88581329cad76c9d6
AlanGustav0 <alagustavo946@gmail.com>
2023-07-05 08:27:43 -0300

    fix: page-main.component

--- src/app/home/modules/page-main/page-main.component.ts
+++ src/app/home/modules/page-main/page-main.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-main',
   templateUrl: './page-main.component.html',
-  styleUrls: ['./page-main.component.scss']
 })
 export class PageMainComponent {
 
